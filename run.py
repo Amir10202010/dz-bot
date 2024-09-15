b442227c3c94f7d441db9f8c4b606e12e9c22937
Amir <158802195+Amir10202010@users.noreply.github.com>
2024-09-15 17:41:13 +0500

    Update run.py

--- run.py
+++ run.py
@@ -3,20 +3,29 @@ from aiogram import Bot, Dispatcher
 from background import keep_alive
 from config import TOKEN
 from app.handlers import router
+import fonttools
 
-
-bot = Bot(token = TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
 
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
 
+def install_fonts():
+    font_files = [
+        'https://github.com/google/fonts/raw/master/apache/arial/Arial-Regular.ttf',
+        'https://github.com/google/fonts/raw/master/apache/calibri/Calibri-Regular.ttf',
+        'https://github.com/google/fonts/raw/master/apache/timesnewroman/TimesNewRoman-Regular.ttf'
+    ]
+
+    for font_file in font_files:
+        fonttools.install_font(font_file)
 
 if __name__ == '__main__':
     try:
         print('Запуск Бота')
+        install_fonts() 
         keep_alive()
         asyncio.run(main())
     except KeyboardInterrupt:
