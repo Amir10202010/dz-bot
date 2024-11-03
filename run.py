7a2a4fc049784e92177381552606fe72dc362489
Amir <158802195+Amir10202010@users.noreply.github.com>
2024-11-03 22:00:28 +0500

    Update run.py

--- run.py
+++ run.py
@@ -4,8 +4,6 @@ from config import TOKEN
 from app.handlers import router
 from background import keep_alive
 import os
-import zipfile
-import spire.xls
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
@@ -17,10 +15,6 @@ async def main():
 if __name__ == '__main__':
     try:
         print('Запуск Бота')
-        with zipfile.ZipFile("Cloud_fonts.zip","r") as zip_fonts:
-            zip_fonts.extractall(os.getcwd())
-        keep_alive()
-        print(os.listdir(os.getcwd()))
         asyncio.run(main())
     except KeyboardInterrupt:
         print('Завершение Бота')
